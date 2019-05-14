0616f65223aecfdddb184ec2768c69639cc93d16
apurva kulkarni <apurva.kulkarni@conduent.com>
2019-05-14 17:19:46 -0500

    added routes, navigations

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
-class App extends React.Component {
-  render() {
-    return <div>
-      APP
-    </div>
-  }
+const App = () => {
+
+  return (<div className='ui container'>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <Route path="/" exact="exact" component={StreamList}/>
+        <Route path="/streams/new" exact="exact" component={StreamCreate}/>
+        <Route path="/streams/edit" exact="exact" component={StreamEdit}/>
+        <Route path="/streams/delete" exact="exact" component={StreamDelete}/>
+        <Route path="/streams/show" exact="exact" component={StreamShow}/>
+      </div>
+    </BrowserRouter>
+  </div>)
 }
 
 export default App;
