bfc6305a31a814c52e66e09220b83029ffd74ff5
Apurva165 <Apurva165@gmail.com>
2019-05-18 22:34:51 -0500

    redux form completed

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,51 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends React.Component {
+  renderInput = ({ input, label, meta }) => {
+    //const input = formProps.input;
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        <div>{meta.error}</div>
+      </div>
+    );
+  };
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+  onSubmit = formValues => {
+    //console.log({ formValues });
+  };
+  render() {
+    //console.log(this.props);
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field label="Enter Title" name="title" component={this.renderInput} />
+        <Field
+          label="Enter Description"
+          name="description"
+          component={this.renderInput}
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title = "Must Enter Title";
+  }
+  if (!formValues.description) {
+    errors.description = "Must Enter description";
+  }
+  return errors;
 };
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate",
+  validate
+})(StreamCreate);
